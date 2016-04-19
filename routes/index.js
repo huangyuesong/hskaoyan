dd1bdf0099800a37c6dfae6780b1d0a5361465f9
huangyuesong <huangyuesong@meituan.com>
2016-04-19 16:16:06 +0800

    develop app download page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/app', function(req, res, next) {
+  res.render('AppDownload');
 });
 
 module.exports = router;
