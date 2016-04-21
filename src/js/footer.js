9520dc7af6f2dc4db31a8f58488a81d0c3494599
huangyuesong <huangyuesong@meituan.com>
2016-04-21 09:53:39 +0800

    header & footer done

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,5 +1,15 @@
 import '../styles/footer.scss';
 
 export default $(function () {
-    console.log('footer.js')
+    let footer = [
+    	'<div class="footer">',
+    	'	<p>咨询热线：010-52899685&nbsp;&nbsp;13681568652&nbsp;&nbsp;15810695171</p>',
+    	'	<p>网络课堂报名电话：13681568652&nbsp;&nbsp;网络课堂技术支持：010-52899685</p>',
+    	'	<p>版权归属：慧升教育&nbsp;&nbsp;京ICP备</p>',
+    	'	<p>总部地址：北京市海淀区北四环中路238号柏彦大厦501</p>',
+    	'	<p>老师咨询QQ：李老师&nbsp;&nbsp;王老师&nbsp;&nbsp;张老师&nbsp;&nbsp;宗老师&nbsp;&nbsp;陈老师</p>',
+    	'</div>',
+    ].join('');
+
+    $('script', $('body')).before($(footer));
 });
