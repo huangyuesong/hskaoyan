9daef6806c22548e1821a3dcc589117860e71f01
huangyuesong <huangyuesong@meituan.com>
2016-04-22 18:06:37 +0800

    html & css & partial js of region section of forum.html

--- src/js/forum.js
+++ src/js/forum.js
@@ -4,18 +4,63 @@ import './header';
 
 import './footer';
 
-let region = function () {
+let schools = [];
+for (let i = 0; i < 22; ++i) {
+	schools.push({
+		name: '北京大学',
+		forumLink: '#',
+		newsLink: '#',
+		dataLink: '#',
+	});
+}
+
+let School = function (data) {
+	return $([
+		`<div class="school">`,
+		`	<div class="name-wrapper"><a href="#" title="${data.name || '清华大学'}">${data.name || '清华大学'}</a></div>`,
+		`	<a href="${data.forumLink || 'javascript:void(0)'}" class="link">论坛</a>`,
+		`	<a href="${data.newsLink || 'javascript:void(0)'}" class="link">资讯</a>`,
+		`	<a href="${data.dataLink || 'javascript:void(0)'}" class="link">资料</a>`,
+		`</div>`,
+	].join(''));
+};
+
+let Row = function (schools) {
+	let _row = $('<div class="row"></div>');
+
+	schools.map((school)=> {
+		_row.append(new School({
+			name: school.name,
+			forumLink: school.forumLink,
+			newsLink: school.newsLink,
+			dataLink: school.dataLink,
+		}));
+	});
+
+	return _row;
+};
+
+let Region = function (data) {
 	return $([
 		'<div class="section region">',
 		'	<div class="title">',
-		'		<span>北京</span>',
+		`		<span>${data.region || '北京'}</span>`,
 		'	</div>',
-		'	<div class="content">',
-		'	</div>',
+		'	<div class="content"></div>',
 		'</div>',
 	].join(''));
 };
 
 $(()=> {
-	$('.container').append(new region());
+	$('.section + .region').remove();
+
+	let region = new Region({
+		region: '北京',
+	});
+
+	while (schools.length) {
+		$('.content', region).append(new Row(schools.splice(0, 8)));
+	}
+
+	$('.container').append(region);
 });
