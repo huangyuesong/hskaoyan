f92e78123a0da72739e1333dbc17624b9f4b12b9
huangyuesong <huangyuesong@meituan.com>
2016-05-10 10:37:55 +0800

    personal_center.html#setting OK

--- src/js/personal_center.js
+++ src/js/personal_center.js
@@ -3,3 +3,35 @@ import '../styles/personal_center.scss';
 import './component/footer';
 
 import 'amazeui';
+
+import url from 'url';
+
+let section = url.parse(location.href, true).hash.substring(1) || 'setting';
+
+$(window).load(()=> {
+	$('.loading').remove();
+});
+
+class PersonalCenter {
+	constructor () {
+		this.model = {};
+		this.controller = {
+			bindEvent: ()=> {},
+		};
+		this.view = {};
+	}
+
+	init () {
+		$('.container .main .nav ul li').each((idx, li)=> {
+			$(li).prop('class').indexOf(section) === -1 ? (()=> null)() : $(li).addClass('active');
+		});
+
+		$('.container .main .am-tabs').each((idx, tabs)=> {
+			$(tabs).prop('class').indexOf(section) === -1 ? (()=> null)() : $(tabs).css({display: 'block'});
+		});
+	}
+}
+
+$(()=> {
+	new PersonalCenter().init();
+});
